0402efbb92d38575ffc4907f5ee8ab34e2fd4c7d
bgeVam <georg.bernold@gmx.at>
2016-09-15 14:06:20 +0200

    Add post-install-script

---
+++ post-install-script.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+clear
+
+read -p "Press [ENTER] to set up elementary OS..."
+
+# Add Repositories
+sudo apt-add-repository \
+ppa:qos/pulseaudio-dlna \
+ppa:webupd8team/sublime-text-3 \
+ppa:alessandro-strada/ppa \
+
+
+# Update Repositories
+sudo apt-get update
+
+# Install Packages
+sudo apt-get install -f \
+pulseaudio-dlna \
+openjdk-8-jdk \
+chromium-browser \
+chromium-codecs-ffmpeg \
+chromium-codecs-ffmpeg-extra \
+sublime-text-installer \
+google-drive-ocamlfuse \
+solaar-gnome3 \
+
+
+
+# Post Installation Action
+apt-cache search jdk
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk
+export PATH=$PATH:/usr/lib/jvm/java-8-openjdk/bin
+google-drive-ocamlfuse
+sudo mkdir -p "/home/georg/Google Drive"
+google-drive-ocamlfuse "/home/georg/Google Drive"
